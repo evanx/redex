57d9e3fd905606c0ac02c584cea3e305a8b29f63
Evan Summers <evans@saratoga.co.za>
2015-06-01 23:24:25 +0200

    async for HttpGet

--- processors/RedisHttpRequestImporter.js
+++ processors/RedisHttpRequestImporter.js
@@ -12,17 +12,21 @@ export default class RedisHttpRequestImporter {
    constructor(config) {
       assert(config.queue.in, 'queue.in');
       assert(config.queue.out, 'queue.out');
+      assert(config.queue.pending, 'queue.pending');
       assert(config.route, 'route');
       this.config = config;
       logger.info('constructor', this.constructor.name, this.config);
       this.seq = 0;
       this.popTimeout = this.config.popTimeout || 0;
       this.redisBlocking = new Redis();
-      this.dispatch();
+      this.pop();
    }
 
-   dispatch() {
-      this.redisBlocking.brpop(this.config.queue.in, this.popTimeout).then(redisReply => {
+   async pop() {
+      try {
+         const redisReply = await this.redisBlocking.brpoplpush(this.config.queue.in,
+            this.config.queue.pending, this.popTimeout);
+         this.addedPending(redisReply);
          this.seq += 1;
          logger.debug('redisReply:', redisReply);
          let data = JSON.parse(redisReply[1]);
@@ -31,11 +35,13 @@ export default class RedisHttpRequestImporter {
          let message = { data, redixInfo };
          logger.info('pop:', message);
          redix.dispatchMessage(this.config, message, this.config.route);
-         this.dispatch();
-      }).catch(error => {
+         this.pop();
+      } catch(error) {
          logger.error('error:', error, error.stack);
-         setTimeout(this.dispatch, config.errorWaitMillis || 1000);
-      });
+         setTimeout(this.pop, config.errorWaitMillis || 1000);
+      } finally {
+         this.removePending(redisReply);
+      }
    }
 
    processReply(reply) {
