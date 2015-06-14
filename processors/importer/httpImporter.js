a38c87285e4f81c8bf7c0a50e38aad1f31697946
Evan Summers <evans@saratoga.co.za>
2015-06-14 07:39:22 +0200

    scripts

--- processors/importer/httpImporter.js
+++ processors/importer/httpImporter.js
@@ -35,8 +35,7 @@ export default function httpImporter(config, redix) { // trying processor constr
       logger.info('req', req.url, Object.keys(req).toString());
       try {
          seq += 1;
-         let meta = {type: 'express', id: seq};
-         req.host = req.headers.host;
+         let meta = {type: 'express', id: seq, host: req.host};
          let response = await redix.import(req, meta, config);
          assert(response, 'response');
          assert(response.statusCode, 'statusCode');
