6386369e04d2ce8cf9e5512c08428f9012a0a311
Evan Summers <evans@saratoga.co.za>
2015-06-16 18:12:18 +0200

    start config

--- configurators/httpFileServer.js
+++ configurators/httpFileServer.js
@@ -1,30 +1,29 @@
 
 import assert from 'assert';
 
-const importer = "importer.httpImporter.singleton";
-const translator = "translator.expressFile.singleton";
-const fileServer = "server.fileServer.singleton";
-
 export default function createConfigs(config, redixConfig) {
-   const port = config.port || 8880;
-   const root = config.root || '.';
-   const index = config.index || 'index.html';
-   const timeout = config.timeout || 2000;
-   return Object.assign({port, root, index, timeout}, {
-      processors: [
-         Object.assign({port, timeout}, {
-            processorName: importer,
-            description: "Express webserver to import HTTP requests",
-            route: [ translator, fileServer ]
-         }),
-         {
-            processorName: translator,
-            description: "Translate ExpressJS 'http' message to 'file' message"
-         },
-         Object.assign({root, index}, {
-            processorName: fileServer,
-            description: "Serve files for a webserver"
-         }
-      ]
+   const names = {
+      importer: 'importer.httpImporter.singleton',
+      translator: 'translator.expressFile.singleton',
+      fileServer: 'server.fileServer.singleton'
    };
+   return [
+      {
+         processorName: names.importer,
+         description: "Express webserver to import HTTP requests",
+         port: config.port || 8880,
+         timeout: config.timeout || 2000
+         route: [ names.translator, names.fileServer ]
+      },
+      {
+         processorName: names.translator,
+         description: "Translate ExpressJS 'http' message to 'file' message"
+      },
+      {
+         processorName: names.fileServer,
+         description: "Serve files for a webserver"
+         root: config.root || '.',
+         index: config.index || 'index.html',
+      }
+   ]
 }
