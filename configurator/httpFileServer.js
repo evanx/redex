3127338bae02ec394ff2058bdaecd72129b18270
Evan Summers <evans@saratoga.co.za>
2015-06-17 03:13:41 +0200

    all test

--- configurator/httpFileServer.js
+++ configurator/httpFileServer.js
@@ -1,29 +1,34 @@
 
 import assert from 'assert';
 
-export default function(config, redixConfig) {
-   const names = {
-      importer: 'importer.httpImporter.singleton',
-      translator: 'translator.expressFile.singleton',
-      fileServer: 'server.fileServer.singleton'
+const processorNames = {
+   importer: "importer.httpImporter.singleton",
+   translator: "translator.expressFile.singleton",
+   fileServer: "server.fileServer.singleton"
+};
+
+export default function createConfigs(config, redixConfig) {
+   const configuration = {
+      port: config.port || 8880,
+      root: config.root || '.',
+      index: config.index || 'index.html',
+      timeout: config.timeout || 2000
    };
-   return [
-      {
-         processorName: names.importer,
-         description: "Express webserver to import HTTP requests",
-         port: config.port || 8880,
-         timeout: config.timeout || 2000,
-         route: [ names.translator, names.fileServer ]
-      },
-      {
-         processorName: names.translator,
-         description: "Translate ExpressJS 'http' message to 'file' message"
-      },
-      {
-         processorName: names.fileServer,
-         description: "Serve files for a webserver",
-         root: config.root || '.',
-         index: config.index || 'index.html'
-      }
-   ]
+   return Object.assign(configuration, {
+      processors: [
+         Object.assign({port, timeout}, {
+            processorName: processorNames.importer,
+            description: "Express webserver to import HTTP requests",
+            route: [ processorNames.translator, processorNames.fileServer ]
+         }),
+         {
+            processorName: processorNames.translator,
+            description: "Translate ExpressJS 'http' message to 'file' message"
+         },
+         Object.assign({root, index}, {
+            processorName: processorNames.fileServer,
+            description: "Serve files for a webserver"
+         }
+      ]
+   };
 }
