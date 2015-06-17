08f77d46eb3fb29728008a278b680277596de8f7
Evan Summers <evans@saratoga.co.za>
2015-06-17 03:18:50 +0200

    all test

--- configurator/httpFileServer.js
+++ configurator/httpFileServer.js
@@ -2,25 +2,25 @@
 import assert from 'assert';
 
 export default function createConfigs(config, redixConfig) {
-   const processorNames = {
+   const names = {
       importer: "importer.httpImporter.singleton",
       translator: "translator.expressFile.singleton",
       fileServer: "server.fileServer.singleton"
    };
    return [
       {
-         processorName: processorNames.importer,
+         processorName: names.importer,
          description: "Express webserver to import HTTP requests",
          port: config.port || 8880,
          timeout: config.timeout || 2000,
-         route: [ processorNames.translator, processorNames.fileServer ]
+         route: [ names.translator, names.fileServer ]
       }),
       {
-         processorName: processorNames.translator,
+         processorName: names.translator,
          description: "Translate ExpressJS 'http' message to 'file' message"
       },
       {
-         processorName: processorNames.fileServer,
+         processorName: names.fileServer,
          description: "Serve files for a webserver",
          root: config.root || '.',
          index: config.index || 'index.html'
