b9d6af9fbe0fbc0a92fc976002b4bb1dcd5b53d4
Evan Summers <evans@saratoga.co.za>
2015-06-21 19:00:47 +0200

    scripts

--- configurator/httpFileServer.js
+++ configurator/httpFileServer.js
@@ -2,8 +2,9 @@
 import assert from 'assert';
 
 // A configurator is for a given pattern, e.g. a simple static web server in this case.
-// It takes a wholistic config for that pattern e.g. port and document root,
-// and generates an array of processor configs e.g. a HTTP server (with the specified port),
+// It takes a wholistic config for that pattern e.g. port and document root.
+// Using that config, it generates an array of processor configs
+// e.g. a HTTP server (with the specified port),
 // and a file reader (with the specified document root)
 
 export default function createConfigs(config, redexConfig) {
