67f45af11d126ee059ac96882acc421c51c1fe32
Evan Summers <evan.summers@gmail.com>
2015-06-22 22:04:36 +0200

    scripts

--- processors/http/cache/redis.js
+++ processors/http/cache/redis.js
@@ -6,16 +6,17 @@ import assert from 'assert';
 import bunyan from 'bunyan';
 import util from 'util';
 
+const Millis = requireRedex('lib/Millis');
 const Redis = requireRedex('lib/Redis');
 
 export default function httpCacheRedis(config, redex, logger) {
 
    assert(config.redisKey, 'redisKey');
-   assert(config.expiry, 'expiry');
+   config.expire = Millis.assert(config.expire, 'expire');
 
    const redis = new Redis();
 
-   logger.info('started');
+   logger.info('started', config.redisKey, Millis.format(config.expire), config.expire);
 
    const service = {
       get state() {
@@ -24,16 +25,22 @@ export default function httpCacheRedis(config, redex, logger) {
       async process(message, meta, route) {
          assert.equal(meta.type, 'express', 'http message type: ' + meta.type);
          assert(message.url, 'message url: ' + meta.type);
-         let redisKey = config.redisKey + ':' + message.url;
-         logger.info('process', redisKey);
-         const cached = await redis.get(redisKey);
-         if (cached !== null) {
-            return cached;
+         let redisKey = config.redisKey + ':hashes:' + message.url;
+         logger.info('redisKey', redisKey);
+         if (config.clear) {
+           await redis.del(redisKey);
+         } else {
+           const cached = await redis.hgetall(redisKey);
+           if (cached !== null) {
+             logger.info('cached', cached);
+             return cached;
+           }
          }
          return redex.dispatch(message, meta, route).then(reply => {
             assert(reply.statusCode, 'reply statusCode');
             if (reply.statusCode === 200) {
-               redis.set(config.redisKey + ':' + message.url);
+               redis.hmset(redisKey, reply);
+               redis.expire(redisKey, config.expire);
             }
             return reply;
          });
