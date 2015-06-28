ce57a47b333ce9d87dd9ea0f89fab5adf1dc5ffc
Evan Summers <evans@saratoga.co.za>
2015-06-28 05:27:43 +0200

    scripts

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -8,12 +8,14 @@ fi
 c1commit() {
   message="$1"
   cd util 
+  pwd
   git add --all 
   git commit -m "$message" 
   git push
-  echo; pwd
   git status
   cd ..
+  echo
+  pwd
   git add --all  
   git commit -m "$message" 
   git push
