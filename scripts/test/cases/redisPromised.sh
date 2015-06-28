385a30a17c89bfef3189f8361c097139db759e1a
Evan Summers <evans@saratoga.co.za>
2015-06-28 06:05:14 +0200

    scripts

--- scripts/test/cases/redisPromised.sh
+++ scripts/test/cases/redisPromised.sh
@@ -1,4 +1,4 @@
 
 pwd | grep -q '/redex$' || exit 1
 
-  ( babel-node test/redisPromised.js && echo "$0 OK" | bunyan -o short
+ ( babel-node test/redisPromised.js && echo "$0 OK" ) | bunyan -o short
