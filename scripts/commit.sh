c3ebb03ff2aea476496252a122dd9dd609233f9c
Evan Summers <evans@saratoga.co.za>
2015-07-06 02:40:51 +0200

    scripts

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,22 +5,23 @@ then
   exit 1
 fi
 
-c1commit() {
-  message="$1"
-  cd util 
+c1push() {
   pwd
   git pull
   git add --all 
   git commit -m "$message" 
   git push
   git status
+}
+
+c1commit() {
+  message="$1"
+  c1push $message
+  cd util 
+  c1push $message
   cd ..
   pwd
-  git pull
-  git add --all  
-  git commit -m "$message" 
-  git push
-  git status
+  c1push $message
 }
 
 
